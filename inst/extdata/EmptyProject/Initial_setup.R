3132146f71e1a77f2d767aa916440ac6564c403e
stepaol1 <ollegstepanov@gmail.com>
2019-02-04 10:09:51 +0300

    delete Initial_setup.R

--- inst/extdata/EmptyProject/Initial_setup.R
+++
@@ -1,7 +0,0 @@
-#LIbrary configuration
-packrat::init()
-
-if(!require(devtools)) {
-  install.packages("devtools"); require(devtools)} #load / install+load devtools
-if(!require(MSDproject)) {
-  install_github("Ollegst/MSDproject", auth_token = "3ecaeb268175d8b4dd02323c9be66ba90e1da6a6"); require(MSDproject)} #load / install+load devtools
