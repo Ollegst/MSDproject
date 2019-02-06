b418da6e96eedfee894fb0aae1cf18b708834769
stepaol1 <ollegstepanov@gmail.com>
2019-02-06 10:45:42 +0300

    Add ignoring IQR in profile and instalation instructions in initial setup

--- inst/extdata/EmptyProject/Initial_setup.R
+++ inst/extdata/EmptyProject/Initial_setup.R
@@ -3,3 +3,9 @@ if(!require(devtools)) {
   install.packages("devtools"); require(devtools)} #load / install+load devtools
 if(!require(MSDproject)) {
   install_github("Ollegst/MSDproject"); require(MSDproject)} #load / install+load devtools
+
+# If you need IQRtools, follow next commands:
+source("https://iqrtools.intiquan.com/install.R")
+installVersion.IQRtools(forceDependencies=TRUE) 
+library(IQRtools)
+test_IQRtools()
